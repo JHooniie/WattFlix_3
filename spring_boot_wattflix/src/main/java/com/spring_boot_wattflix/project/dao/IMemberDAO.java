01648c29d6cf936ab03ea3b0c9720be8648b26d5
slowlyseo <tjwlgus127@gmail.com>
2022-07-20 17:18:29 +0900

    deleteMember_mvc: Add 회원탈퇴 기능 구현중

--- spring_boot_wattflix/src/main/java/com/spring_boot_wattflix/project/dao/IMemberDAO.java
+++ spring_boot_wattflix/src/main/java/com/spring_boot_wattflix/project/dao/IMemberDAO.java
@@ -4,18 +4,17 @@ import java.util.HashMap;
 
 import com.spring_boot_wattflix.project.model.MemberVO;
 
-
 public interface IMemberDAO {
 	// 회원가입
-	public void insertMember (MemberVO memVo);
-	String memIdCheck (String memId);
-	
+	public void insertMember(MemberVO memVo);
+
+	String memIdCheck(String memId);
+
 	// 로그인
 	String loginCheck(HashMap<String, Object> map);
-	
+
 	// 회원소개/정보 수정
-	
-	
+
 	// 회원 탈퇴
-	public void dropMember (String memNo);
+	public void deleteMember(String memId);
 }
--- spring_boot_wattflix/src/main/java/com/spring_boot_wattflix/project/service/IMemberService.java
+++ spring_boot_wattflix/src/main/java/com/spring_boot_wattflix/project/service/IMemberService.java
@@ -6,15 +6,15 @@ import com.spring_boot_wattflix.project.model.MemberVO;
 
 public interface IMemberService {
 	// 회원가입
-	public void insertMember (MemberVO memVo);
-	String memIdCheck (String memId);
-	
+	public void insertMember(MemberVO memVo);
+
+	String memIdCheck(String memId);
+
 	// 로그인
 	String loginCheck(HashMap<String, Object> map);
-	
+
 	// 회원소개/정보 수정
-	
-	
+
 	// 회원 탈퇴
-	public void dropMember (String memNo);
+	public void deleteMember(String memId);
 }
