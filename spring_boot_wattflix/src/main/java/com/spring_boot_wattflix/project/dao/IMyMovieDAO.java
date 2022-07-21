b4708289dd43fe7a238d15afb6d76d0d67dffd0c
saehim <saeilee.vxv@gmail.com>
2022-07-21 09:45:54 +0900

    myMovie_java,xml Add: 마이무비 기능 첫번째 구현

---
+++ spring_boot_wattflix/src/main/java/com/spring_boot_wattflix/project/dao/IMyMovieDAO.java
@@ -0,0 +1,22 @@
+package com.spring_boot_wattflix.project.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.spring_boot_wattflix.project.model.MyMovieVO;
+
+public interface IMyMovieDAO {
+	// 비회원 북마크 회원가입 유도
+	
+	// 회원 마이무비 추가
+	public void insertMymovie(MyMovieVO mmovie);
+	
+	// 마이무비 조회
+	public ArrayList<MyMovieVO> listAllMymovie(String memId);
+	
+	// 마이무비 삭제
+	public void deleteMymovie(HashMap<String, Object> map);
+
+	public int checkMymovie(HashMap<String, Object> map);
+	
+}
